2ab94ae6985b73b0044ecff1f480d3216a123263
aishwarya <ashp17star@gmail.com>
2026-05-22 21:48:05 +0530

    higher order functions

--- lambda_function.py
+++ lambda_function.py
@@ -9,18 +9,3 @@ def max_of_numbers(n1,n2):
         return n1
     else:
         return n2
-
-#filter function -- higher order function
-def chk_positive(n):
-    if n > 0:
-        return True
-    else:
-        return False
-
-list_1 = [34,-1,-3,0,97,66]
-filtered_list = list(filter(chk_positive,list_1))
-print(filtered_list)
-
-#filter with lambda functions
-filtered_list_2 = list(filter(lambda i : True if i > 0 else False,list_1))
-print(filtered_list_2)
